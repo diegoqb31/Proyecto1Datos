33a42b488551c73cbbf7d6e591f4dac074d9c561
Diego Quirós <quirosbdiego@gmail.com>
2021-09-18 19:00:01 -0600

    Quitando comentarios

--- Proyecto1/ListaDoblementeEnlazada.h
+++ Proyecto1/ListaDoblementeEnlazada.h
@@ -23,12 +23,6 @@ public:
 
 	typedef Nodo* listaptr;
 
-	/*bool listaVacia();
-	bool agregaFinal(T*);
-	string imprimir();
-	void borrarTodo();
-	~listaGenerica();
-	int getSize();*/
 
 
 	ListaDoblementeEnlazada() {
@@ -75,94 +69,4 @@ public:
 		}
 	}
 
-	
-//
-//
-//template<class T>
-//bool listaGenerica<T>::agregaFinal(T* dato)
-//{
-//	nodo<T>* _nodo = new nodo<T>(dato);
-//	nodo<T>* temp = primero;
-//
-//	if (vacio())
-//	{
-//		primero = _nodo;
-//	}
-//	else
-//	{
-//		while (temp->getSig())
-//		{
-//			temp = temp->getSig();
-//		}
-//		temp->setSig(_nodo);
-//	}
-//	size++;
-//	return true;
-//}
-//
-////--------------------------------------------
-//template<class T>
-//string listaGenerica<T>::toString()
-//{
-//	nodo<T>* temp = primero;
-//	stringstream s;
-//	int i = 1;
-//
-//	while (temp)
-//	{
-//		s << i << " - ";
-//		s << temp->getElemento()->toString() << endl;
-//		temp = temp->getSig();
-//		i++;
-//	}
-//	return s.str();
-//}
-//
-////--------------------------------------------
-//template<class T>
-//listaGenerica<T>::~listaGenerica()
-//{
-//	borrarTodo();
-//}
-//
-////--------------------------------------------
-//template<class T>
-//void listaGenerica<T>::borrarTodo()
-//{
-//
-//	nodo<T>* temp = primero;
-//	size = 0;
-//	while (temp != NULL)
-//	{
-//		temp = primero;
-//		primero = primero->getSig();
-//		delete temp;
-//	}
-//}
-////--------------------------------------------------------------
-//
-//
-//template<class T>
-//T* listaGenerica<T>::operator[](int indice) {
-//	T* dato;
-//	nodo<T>* temp = primero;
-//	int i = 0;
-//
-//	if (indice > size)
-//	{
-//		return NULL;
-//	}
-//	while (i < indice) {
-//		temp = temp->getSig();
-//		i++;
-//	}
-//
-//	dato = temp->getElemento();
-//	return dato;
-//}
-//
-//template<class T>
-//int listaGenerica<T>::getSize() {
-//	return size;
-//}
 };
